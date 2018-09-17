33b15d9018502029e8f719813ca26cd4a3ff2f03
chenyinxin <chenyinxin@163.com>
2018-09-17 14:41:21 +0800

    20180917

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/wwwroot/js/mobile/common/validate/test/additional/creditcard.js
+++
@@ -1,7 +0,0 @@
-test( "creditcard", function() {
-	var method = methodTest( "creditcard" );
-	ok( method( "4111-1111-1111-1111" ), "Valid creditcard number" );
-	ok( method( "4111 1111 1111 1111" ), "Valid creditcard number" );
-	ok( !method( "41111" ), "Invalid creditcard number" );
-	ok( !method( "asdf" ), "Invalid creditcard number" );
-} );
